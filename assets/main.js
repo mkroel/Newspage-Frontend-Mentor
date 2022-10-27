fcafb3abfb62acc0d0ea8aa7dd746dc93a2809a4
mkroel <105654571+mkroel@users.noreply.github.com>
2022-10-27 20:18:16 +0200

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+function openMenu() {
+    var x = document.getElementById("menu");
+        
+        if (x.style.display === "none") {
+        x.style.display = "block";
+        } else {
+        x.style.display = "none";
+        }
+}
+
+function closeMenu() {
+    var y = document.getElementById("menu");
+
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        } else {
+        y.style.display = "none";
+        }
+}
